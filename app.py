9840b833b9e71d78f1cf8bce1ffb15764e522e85
alaeddine-hash <alaeddinemansouri2@gmail.com>
2024-02-13 16:22:00 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,78 @@
+import dotenv
+import requests
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Weaviate
+import weaviate
+from weaviate.embedded import EmbeddedOptions
+from langchain.prompts import ChatPromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.schema.runnable import RunnablePassthrough
+from langchain.schema.output_parser import StrOutputParser
+
+
+
+
+dotenv.load_dotenv()
+#downloading the document from the internet
+url = "https://raw.githubusercontent.com/langchain-ai/langchain/master/docs/docs/modules/state_of_the_union.txt"
+res = requests.get(url)
+with open("state_of_the_union.txt", "w") as f:
+    f.write(res.text)
+
+#loading the document from the file
+loader = TextLoader('./state_of_the_union.txt')
+documents = loader.load()
+
+#splitting the document into chunks
+text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+chunks = text_splitter.split_documents(documents)
+
+#embedding the chunks
+client = weaviate.Client(
+  embedded_options = EmbeddedOptions()
+)
+
+vectorstore = Weaviate.from_documents(
+    client = client,    
+    documents = chunks,
+    embedding = OpenAIEmbeddings(),
+    by_text = False
+)
+
+#retrieving the chunks
+retriever = vectorstore.as_retriever()
+
+
+#prompt
+template = """You are an assistant for question-answering tasks. 
+Use the following pieces of retrieved context to answer the question. 
+If you don't know the answer, just say that you don't know. 
+Use three sentences maximum and keep the answer concise.
+Question: {question} 
+Context: {context} 
+Answer:
+"""
+prompt = ChatPromptTemplate.from_template(template)
+
+print(prompt)
+
+
+#chat model
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+
+
+#output parser
+rag_chain = (
+    {"context": retriever,  "question": RunnablePassthrough()} 
+    | prompt 
+    | llm
+    | StrOutputParser() 
+)
+
+#invoke the chain
+query = "What did the president say about Justice Breyer"
+rag_chain.invoke(query)
+
+
